ac6b4998d67bc59c41ab0e245b4775ff24ff5a92
Takeshi Matsuoka <tachesimazzoca@gmail.com>
2013-07-24 14:29:38 +0900

    Update RegexExample

--- src/main/java/com/github/tachesimazzoca/java/example/RegexExample.java
+++ src/main/java/com/github/tachesimazzoca/java/example/RegexExample.java
@@ -1,5 +1,6 @@
 package com.github.tachesimazzoca.java.example;
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public final class RegexExample {
@@ -7,12 +8,21 @@ public final class RegexExample {
     }
 
     public static void main(String[] args) {
-        Pattern ptn = Pattern.compile("[0-9a-zA-Z]+");
-        System.out.println(ptn.matcher("DEADBEEEF").matches());
-        System.out.println(ptn.matcher("DEADBEEEF!").matches());
-
         final String ptnStr = "[0-9a-zA-Z]+";
         System.out.println("DEADBEEF".matches(ptnStr));
         System.out.println("DEADBEEF!".matches(ptnStr));
+
+        Pattern ptn = Pattern.compile("[0-9a-zA-Z]+");
+        System.out.println(ptn.matcher("DEADEEEF").matches());
+        System.out.println(ptn.matcher("DEADEEEF!").matches());
+
+        Pattern urlPattern = Pattern.compile("(https?)://([^/]+)(.*)");
+        Matcher urlMatcher = urlPattern.matcher("http://www.example.net/foo");
+        if (urlMatcher.matches()) {
+            int c = urlMatcher.groupCount();
+            for (int i = 0; i <= c; i++) {
+                System.out.println(urlMatcher.group(i));
+            }
+        }
     }
 }

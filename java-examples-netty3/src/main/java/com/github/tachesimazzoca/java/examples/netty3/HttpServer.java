52f9b2bcb48e0e0b72df0885616768b542b39329
Takeshi Matsuoka <tachesimazzoca@gmail.com>
2015-01-19 19:13:30 +0900

    Add HttpServer
    - It currently responds only 503 error

---
+++ java-examples-netty3/src/main/java/com/github/tachesimazzoca/java/examples/netty3/HttpServer.java
@@ -0,0 +1,41 @@
+package com.github.tachesimazzoca.java.examples.netty3;
+
+import org.jboss.netty.bootstrap.ServerBootstrap;
+import org.jboss.netty.channel.ChannelFactory;
+import org.jboss.netty.channel.ChannelPipeline;
+import org.jboss.netty.channel.ChannelPipelineFactory;
+import org.jboss.netty.channel.Channels;
+import org.jboss.netty.channel.socket.nio.NioServerSocketChannelFactory;
+import org.jboss.netty.handler.codec.http.HttpRequestDecoder;
+import org.jboss.netty.handler.codec.http.HttpResponseEncoder;
+
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
+
+public class HttpServer {
+    public static void main(String[] args) {
+        ChannelFactory factory = new NioServerSocketChannelFactory(
+                Executors.newCachedThreadPool(), // boss
+                Executors.newCachedThreadPool()  // worker
+        );
+        ServerBootstrap bootstrap = new ServerBootstrap(factory);
+        bootstrap.setPipelineFactory(new HttpServerPipelineFactory());
+        bootstrap.setOption("child.tcpNoDelay", true);
+        bootstrap.setOption("child.keepAlive", true);
+        bootstrap.bind(new InetSocketAddress(9000));
+    }
+
+    public static class HttpServerPipelineFactory implements ChannelPipelineFactory {
+        @Override
+        public ChannelPipeline getPipeline() throws Exception {
+            ChannelPipeline newPipeline = Channels.pipeline();
+            // netty.handler.codec.http
+            newPipeline.addLast("decoder", new HttpRequestDecoder());
+            newPipeline.addLast("encoder", new HttpResponseEncoder());
+
+            newPipeline.addLast("handler", new HttpServerHandler());
+
+            return newPipeline;
+        }
+    }
+}
---
+++ java-examples-netty3/src/main/java/com/github/tachesimazzoca/java/examples/netty3/HttpServerHandler.java
@@ -0,0 +1,31 @@
+package com.github.tachesimazzoca.java.examples.netty3;
+
+import org.jboss.netty.channel.ChannelFutureListener;
+import org.jboss.netty.channel.ChannelHandlerContext;
+import org.jboss.netty.channel.ExceptionEvent;
+import org.jboss.netty.channel.MessageEvent;
+import org.jboss.netty.channel.SimpleChannelHandler;
+import org.jboss.netty.handler.codec.http.DefaultHttpResponse;
+import org.jboss.netty.handler.codec.http.HttpHeaders;
+import org.jboss.netty.handler.codec.http.HttpRequest;
+import org.jboss.netty.handler.codec.http.HttpResponseStatus;
+import org.jboss.netty.handler.codec.http.HttpVersion;
+
+public class HttpServerHandler extends SimpleChannelHandler {
+    @Override
+    public void messageReceived(ChannelHandlerContext ctx, MessageEvent e) {
+        Object msg = e.getMessage();
+        if (msg instanceof HttpRequest) {
+            System.out.println((HttpRequest) msg);
+        }
+        throw new IllegalArgumentException("Server Error");
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, ExceptionEvent e) {
+        DefaultHttpResponse response = new DefaultHttpResponse(
+                HttpVersion.HTTP_1_1, HttpResponseStatus.SERVICE_UNAVAILABLE);
+        response.headers().set(HttpHeaders.Names.CONNECTION, "close");
+        ctx.getChannel().write(response).addListener(ChannelFutureListener.CLOSE);
+    }
+}

d2195bba91958e91028cbac7c6d06e3d6b322447
Takeshi Matsuoka <tachesimazzoca@gmail.com>
2015-07-05 14:27:46 +0900

    Add axis.ws.HelloApi

---
+++ java-examples-axis/src/main/java/com/github/tachesimazzoca/java/examples/axis/ws/HelloApi.java
@@ -0,0 +1,11 @@
+package com.github.tachesimazzoca.java.examples.axis.ws;
+
+public class HelloApi {
+    public int add(int arg0, int arg1) {
+        return arg0 + arg1;
+    }
+
+    public int abs(int arg0, int arg1) {
+        return Math.abs(arg0 - arg1);
+    }
+}

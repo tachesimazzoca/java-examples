72ac7de6adfc254802e136c4792ff92acbfc9799
Takeshi Matsuoka <tachesimazzoca@gmail.com>
2015-10-12 13:26:22 +0900

    Add java-examples-javafx

---
+++ java-examples-javafx/src/main/java/com/github/tachesimazzoca/java/examples/javafx/HelloApplication.java
@@ -0,0 +1,31 @@
+package com.github.tachesimazzoca.java.examples.javafx;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Hello");
+
+        Button btn = new Button();
+        btn.setText("Say Hello");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                System.out.println(actionEvent.getSource());
+            }
+        });
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root, 300, 250));
+
+        primaryStage.show();
+    }
+}
